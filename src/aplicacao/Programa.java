7a6a42ea336c82c76793d4ffe93e3e5409784c4e
Lucas Diego <luucasdiego_12@hotmail.com>
2022-03-29 17:08:06 -0400

    Junção de arquivos csv

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,23 +1,39 @@
 package aplicacao;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Programa {
-    public static void main(String[] args) {
-        String caminho = "D:\\tabelas\\pesquisa.txt";
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // Caso não exista o arquivo "resultado-pesquisa.csv", será criado automaticamente na pasta.
+        PrintWriter printWriter = new PrintWriter("D:\\tabelas\\resultado-pesquisa.csv");
+        BufferedReader bufferedReader1 = new BufferedReader(new FileReader("D:\\tabelas\\meteorologico.csv"));
+        BufferedReader bufferedReader2 = new BufferedReader(new FileReader("D:\\tabelas\\solo.csv"));
 
         // Bloco "try" instanciando recursos na mesma linha e desalocando de forma automática quando o bloco acabar.
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(caminho))) {
-
-            String linha = bufferedReader.readLine(); // Ler o arquivo até a quebra de linha.
-            while (linha != null) {
-                System.out.println(linha);
-                linha = bufferedReader.readLine();
+        try {
+            // Ler o arquivo até a quebra de linha.
+            String linha1 = bufferedReader1.readLine();
+            String linha2 = bufferedReader2.readLine();
+            while (linha1 != null || linha2 != null) {
+                if (linha1 != null) {
+                    printWriter.println(linha1);
+                    linha1 = bufferedReader1.readLine();
+                }
+                if (linha2 != null) {
+                    printWriter.println(linha1);
+                    linha2 = bufferedReader1.readLine();
+                }
             }
+
+            printWriter.flush();
+            bufferedReader1.close();
+            bufferedReader2.close();
+            printWriter.close();
+
         } catch (IOException e) {
             System.out.println("Erro: " + e.getMessage());
         }
+        System.out.println("Arquivos juntados com sucesso!");
     }
 }
